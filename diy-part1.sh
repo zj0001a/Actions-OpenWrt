aac37d10d4908fc5c35329b8ddf7e459a379e4f4
zj0001a <38984362+zj0001a@users.noreply.github.com>
2022-11-14 13:25:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git alist https://github.com/sbwml/luci-app-alist.git' >>feeds.conf.default
+echo 'src-git vssr https://github.com/jerrykuku/luci-app-vssr.git' >>feeds.conf.default
